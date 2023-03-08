39037fe526dc6c10bb14f2975d82d754144f7fdd
Mohammad Dehghani H <104039622+MohammadDehghaniH@users.noreply.github.com>
2023-03-08 12:33:58 +0100

    Update Fig_12.m

--- Fig_12.m
+++ Fig_12.m
@@ -3,7 +3,6 @@ clc;clear;close all;
 Fig12=figure();
 
 Ntrials_2 = 50000; %Ntrials_2: number of learning cycles
-xax=(1:1:Ntrials_2);
 N_mu=100; %There are \mu independent embedded patterns, 500 simulations (number of simulations)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -14,7 +13,11 @@ hold on
 
 %Data: f_0 = 0.1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Results1=zeros(N_mu,Ntrials_2);
+
+NN=Ntrials_2/10;
+xax=(10:10:Ntrials_2);
+Results1=zeros(N_mu,NN);
+
 for ic=1:N_mu
     try
        eval(['load Learning_BP_f0/Data_ns_15/ER_',num2str(ic),'_',num2str(1),'_',num2str(10),  '.mat ns_15']);
@@ -31,7 +34,10 @@ hold on
 
 %Data: f_0 = 0.02
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Results2=zeros(N_mu,Ntrials_2);
+NN=Ntrials_2/50;
+xax=(50:50:Ntrials_2);
+Results2=zeros(N_mu,NN);
+
 for ic=1:N_mu
     try
        eval(['load Learning_BP_f0/Data_ns_15/ER_',num2str(ic),'_',num2str(2),'_',num2str(50),  '.mat ns_15']);
@@ -50,7 +56,10 @@ hold on
 
 %Data:: f_0 = 0.01
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Results3=zeros(N_mu,Ntrials_2);
+NN=Ntrials_2/100;
+xax=(100:100:Ntrials_2);
+Results3=zeros(N_mu,NN);
+
 for ic=1:N_mu
     try
        eval(['load Learning_BP_f0/Data_ns_15/ER_',num2str(ic),'_',num2str(3),'_',num2str(100),  '.mat ns_15']);
